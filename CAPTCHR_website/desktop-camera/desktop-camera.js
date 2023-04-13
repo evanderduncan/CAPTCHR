7d007944f4cbe4ad6050f3ebd78c2452526b6bf5
evanderduncan <98160485+evanderduncan@users.noreply.github.com>
2023-04-13 20:14:11 +0100

    Add files via upload
    updated desktop camera html, css that's almost working

--- CAPTCHR_website/desktop-camera/desktop-camera.js
+++ CAPTCHR_website/desktop-camera/desktop-camera.js
@@ -2,28 +2,36 @@ window.onload = function() {
     
     
 
-    let video = document.getElementById('video');
-    let canvas = document.getElementById('photo-gallery');
-    let ctx = canvas.getContext('2d');
+  let video = document.getElementById('video');
+  let canvas = document.getElementById('photo-gallery');
+  let ctx = canvas.getContext('2d');
   
-    navigator.mediaDevices.getUserMedia({ video: true })
+  navigator.mediaDevices.getUserMedia({ video: true })
       .then(function(stream) {
-        video.srcObject = stream;
-        video.play();
+          video.srcObject = stream;
+          video.play();
+          
+          canvas.width = video.videoWidth;
+          canvas.height = video.videoHeight;
   
-        canvas.width = video.videoWidth;
-        canvas.height = video.videoHeight;
+          function draw() {
+              ctx.drawImage(video, 0, 0);
+              requestAnimationFrame(draw);
+          }
   
-        function draw() {
-          ctx.drawImage(video, 0, 0);
-          requestAnimationFrame(draw);
-        }
-  
-        video.addEventListener('play', function() {
-          draw();
-        }, false);
+          video.addEventListener('play', function() {
+              draw();
+          }, false);
+          
       })
       .catch(function(err) {
-        console.error('Error accessing webcam: ', err);
+          console.error('Error accessing webcam: ', err);
       });
+  
+  function flashCanvas() {
+      canvas.style.background = "white";
+      setTimeout(function() {
+          canvas.style.background = "";
+      }, 1000);
+  }
   };
